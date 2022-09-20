4f9d6e7231d2ed5f7c2eaf8d929618a0fa87ab8e
Jonathan Martinez <jon@Jonathans-MacBook-Pro.local>
2022-09-20 07:53:24 -0500

    added state to nav and creating the layout of gallery photos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery'
 
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'comerical',
+      description: 'Photos of grocery stores, food trucks, and other commerical projects',
+    },
+    { name: 'portraits', description:'Portraits of people in my life'},
+    { name:'food', description:'Delivious delicacies'},
+    { name: 'landscape', description: ' Fields, farmhouses, waterfalls, and the beauty of nature'},
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+
   return (
   <div>
-  <Nav />
+    <Nav 
+      categories={categories}
+      setCurrentCategory={setCurrentCategory}
+      currentCategory={currentCategory}
+      />
     <main>
+      <Gallery />
       <About />
     </main>
   </div>
