66bd47c46cb70bf0b5bc6ee210b4f4e94defa996
Jonathan Martinez <jon@Jonathans-MacBook-Pro.local>
2022-09-26 15:37:26 -0500

    thumbnail photos are not generating, thumbnails will close and open

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
+import Modal from '../Modal';
+
 
 const PhotoList = ({ category }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [currentPhoto, setCurrentphoto] = useState();
+
     const [photos] = useState([
         {
             name: 'Grocery aisle',
@@ -105,16 +110,24 @@ const PhotoList = ({ category }) => {
     ]);
 
     const currentPhotos = photos.filter((photo) => photo.category === category);
+    const toggleModal = (image, i) => {
+        setCurrentphoto({...image,index: i });
+        setIsModalOpen(!isModalOpen);
+    };
 
 
     return (
-            <div>
+        <div>
+            {isModalOpen && (
+                <Modal onClose={toggleModal} currentPhoto={currentPhoto} />
+            )}
                 <div className="flex-row">
                     {currentPhotos.map((image, i) => (
                         <img 
                             src={require(`../../assets/assets/small/${category}/${i}.jpg`)}
                             alt={image.name}
                             className='img-thumbnail mx-1'
+                            onClick={() => toggleModal(image, i)}
                             key={image.name}
                         />
                     ))}
